18b0913c5dbe9579e5cbe90d61c82baac2b5db31
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-01-04 00:00:08 -0500

    Initial commit

---
+++ appCalculo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  appCalculo
+//
+//  Created by Pierre Juarez U. on 3/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
