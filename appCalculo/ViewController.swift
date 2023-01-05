c821c1b2eebc0831a2a9bbd3f0cf693691a50a09
Pierre Juarez <76144339+pierre-juarez@users.noreply.github.com>
2023-01-05 01:22:04 -0500

    [fix] - On-screen keyboard

--- appCalculo/ViewController.swift
+++ appCalculo/ViewController.swift
@@ -11,7 +11,30 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardUp), name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardDown), name: UIResponder.keyboardWillHideNotification, object: nil)
+    
+  }
+
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    view.endEditing(true) // Close when touching outside the keyboard
+  }
+  
+  @objc func keyboardUp(){
+    let sizeScreen = UIScreen.main.nativeBounds.height
+    if sizeScreen <= 2556 { // Applies only to iPhone, not iPad
+      if self.view.frame.origin.y == 0 {
+        self.view.frame.origin.y = -70
+      }
+    }
+    
+  }
+  
+  @objc func keyboardDown(){
+    if self.view.frame.origin.y != 0 {
+      self.view.frame.origin.y = 0
+    }
   }
 
 
